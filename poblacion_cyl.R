005aa0d1aa28bb342710a00aa17af1ec17cddb96
Paula <pmc1017@alu.ubu.es>
2024-10-01 13:37:08 +0200

    añado un script con el dataframe de la poblacion de cyl

---
+++ poblacion_cyl.R
@@ -0,0 +1,10 @@
+poblacion_cyl <- data.frame(
+  Provincia = c("Ávila", "Burgos", "León", "Palencia", "Salamanca",
+                "Segovia", "Soria", "Valladolid", "Zamora", "Castilla y León"),
+  Poblacion_total = c(159764, 357370, 448573, 157787, 327089, 155332, 89528, 521333, 166927, 2383703),
+  Poblacion_extranjera = c(12868, 33150, 23398, 8581, 18606, 20015, 10363, 33352, 7601, 167934),
+  Porc_pob_extranj_sobre_pob_total = c(8.05, 9.27, 5.21, 5.43, 5.68, 12.88, 11.57, 6.39, 4.55, 7.04),
+  Num_munic_pob_extranj_mas_3_por_ciento = c(106, 220, 102, 88, 130, 167, 83, 150, 110, 1156)
+)
+
+poblacion_cyl
