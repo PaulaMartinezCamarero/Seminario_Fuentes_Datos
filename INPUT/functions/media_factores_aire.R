4bc2651076c80b5664688ffc5c5186989223a05c
Andres <aeu1002@alu.ubu.es>
2024-10-31 14:25:55 +0100

    hago media_factores_aire pero la tenemos que sacar del apartado funciones ya que al emplear tuberias queda mejor fuera de funciones además he probado a cambiar fecha de caracter a date pero el resultado sigue siendo el mismo(ordenado por meses)

---
+++ INPUT/functions/media_factores_aire.R
@@ -0,0 +1,18 @@
+media_facotres_aire <- function(calidad_aire){
+  calidad_aire%>%
+    group_by(Fecha, Provincia)%>%
+    summarise(
+      CO=mean(`CO (mg/m3)`, na.rm = TRUE),
+      NO=mean(`NO (ug/m3)`, na.rm = TRUE),
+      O3=mean(`O3 (ug/m3)`, na.rm = TRUE),
+      PM25=mean(`PM25 (ug/m3)`, na.rm = TRUE)
+      
+    )%>%
+    arrange(Fecha)
+  
+  
+}
+
+
+
+
